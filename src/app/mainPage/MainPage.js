728765ed87e77e488f624a73ebe7174f8928a9b4
Marijo Kis <marijo.kis@q.agency>
2023-12-07 14:43:36 +0100

    chore: first commit

---
+++ src/app/mainPage/MainPage.js
@@ -0,0 +1,192 @@
+import "./MainPage.css";
+
+const MainPage = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <div
+            style={{
+              backgroundColor: "#1068c9",
+              padding: "2px 16px",
+              borderRadius: "8px",
+            }}
+          >
+            <p style={{ color: "white", textAlign: "center" }}>
+              2.5.2024. 17:00h
+            </p>
+          </div>
+          <div
+            style={{
+              padding: "2px 16px",
+            }}
+          >
+            <p>Nasi: - </p>
+            <p>Vasi: -</p>
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              backgroundColor: "#1068c9",
+              padding: "2px 16px",
+              borderRadius: "8px",
+            }}
+          >
+            <p style={{ color: "white", textAlign: "center" }}>
+              3.5.2024. 17:00h
+            </p>
+          </div>
+          <div
+            style={{
+              padding: "2px 16px",
+            }}
+          >
+            <p>Nasi: -</p>
+            <p>Vasi: -</p>
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              backgroundColor: "#4c4b57",
+              padding: "2px 16px",
+              borderRadius: "8px",
+            }}
+          >
+            <p style={{ color: "white", textAlign: "center" }}>
+              1.5.2024. 17:00h
+            </p>
+          </div>
+          <div
+            style={{
+              padding: "2px 16px",
+            }}
+          >
+            <p>Nasi: 4</p>
+            <p>
+              <b>Vasi: 7</b>
+            </p>
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              backgroundColor: "#4c4b57",
+              padding: "2px 16px",
+              borderRadius: "8px",
+            }}
+          >
+            <p style={{ color: "white", textAlign: "center" }}>
+              1.5.2024. 17:00h
+            </p>
+          </div>
+          <div
+            style={{
+              padding: "2px 16px",
+            }}
+          >
+            <p>Nasi: 4</p>
+            <p>
+              <b>Vasi: 7</b>
+            </p>
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              backgroundColor: "#4c4b57",
+              padding: "2px 16px",
+              borderRadius: "8px",
+            }}
+          >
+            <p style={{ color: "white", textAlign: "center" }}>
+              1.5.2024. 17:00h
+            </p>
+          </div>
+          <div
+            style={{
+              padding: "2px 16px",
+            }}
+          >
+            <p>Nasi: 4</p>
+            <p>
+              <b>Vasi: 7</b>
+            </p>
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              backgroundColor: "#4c4b57",
+              padding: "2px 16px",
+              borderRadius: "8px",
+            }}
+          >
+            <p style={{ color: "white", textAlign: "center" }}>
+              1.5.2024. 17:00h
+            </p>
+          </div>
+          <div
+            style={{
+              padding: "2px 16px",
+            }}
+          >
+            <p>Nasi: 4</p>
+            <p>
+              <b>Vasi: 7</b>
+            </p>
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              backgroundColor: "#4c4b57",
+              padding: "2px 16px",
+              borderRadius: "8px",
+            }}
+          >
+            <p style={{ color: "white", textAlign: "center" }}>
+              1.5.2024. 17:00h
+            </p>
+          </div>
+          <div
+            style={{
+              padding: "2px 16px",
+            }}
+          >
+            <p>Nasi: 4</p>
+            <p>
+              <b>Vasi: 7</b>
+            </p>
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              backgroundColor: "#4c4b57",
+              padding: "2px 16px",
+              borderRadius: "8px",
+            }}
+          >
+            <p style={{ color: "white", textAlign: "center" }}>
+              1.5.2024. 17:00h
+            </p>
+          </div>
+          <div
+            style={{
+              padding: "2px 16px",
+            }}
+          >
+            <p>Nasi: 4</p>
+            <p>
+              <b>Vasi: 7</b>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
