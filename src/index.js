e95d20c813bfb5558dc10f71e2b6979e283759a2
Marijo Kis <marijo.kis@q.agency>
2023-12-12 10:25:00 +0100

    chore: test

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/",
+        path: "/volleyball",
         element: <MainPage />,
       },
     ],
